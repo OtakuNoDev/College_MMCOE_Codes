930bd3e9aca5dbcd17d899a09cbd050f87beae61
GJ <gauravjadhav2024.it@mmcoe.edu.in>
2024-12-23 22:05:58 +0530

    Delete Program2.c

--- Program2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("NAME :- Gaurav J. \n \t");
-    printf("College Name :- MMCOE \n \t \t");
-    printf("College ADDRESS :- Karvenagar Pune\n \t \t \t");
-    printf("Subject Name :- EEl\n \t \t \t \t");
-    printf("Subject Code :- B24IT101\n \t \t \t \t \t");
-}
